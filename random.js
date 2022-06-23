98283131934df2af0e23eaf670545ef92fe6a129
officialBakari <66633538+officialBakari@users.noreply.github.com>
2022-06-22 21:59:34 -0400

    Add files via upload

---
+++ random.js
@@ -0,0 +1,15 @@
+let fruits =['avocados','beets', 'carrots', 'dandelions'];
+let removed = fruits.pop();
+console.log(fruits);
+let favoriteFruits = {
+    'a': 'apples',
+    'b': 'bananas',
+    'o': 'oranges',
+    'p o.': 'pairs',
+};
+
+//console.log(Object.value(favoriteFruits));
+
+
+
+//console.log(pets3)
